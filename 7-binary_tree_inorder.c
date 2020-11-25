25f35f692a29dc3f4a63354391997884f7dd69a4
MarcosPimienta <1676@holbertonschool.com>
2020-11-25 22:28:13 +0000

    in-order

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,17 +3,18 @@
 /**
  * binary_tree_inorder -  goes through a binary tree using pre-order traversal
  * @tree: root
+ * @func: print function
  * Return: void
  **/
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (!tree)
-                return;
-        if (func == NULL)
+	if (!tree)
+		return;
+	if (func == NULL)
 		return;
 
-        binary_tree_inorder(tree->left, func);
-        func(tree->n);
-        binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
