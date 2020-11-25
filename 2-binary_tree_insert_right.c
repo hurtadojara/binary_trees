e4ccfe98cb19a0dfeeb63306a99bc2ef13cf2aae
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-11-25 15:57:33 -0500

    fixes

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,14 +14,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-		n_node = binary_tree_node(parent, value);
-		if (!n_node)
-			return (NULL);
-		if (parent->right)
-		{
-			parent->right->parent = n_node;
-			n_node->right = parent->right;
-		}
-			parent->right = n_node;
-			return (n_node);
+	n_node = binary_tree_node(parent, value);
+	if (!n_node)
+		return (NULL);
+	if (parent->right)
+	{
+		parent->right->parent = n_node;
+		n_node->right = parent->right;
+	}
+	parent->right = n_node;
+	return (n_node);
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,11 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (!node)
         return (0);
-    if (!(node->right && node->left) == NULL)
+    if (node->right && node->left)
         return (0);
-    return (1);
+    if (node->left || node->right != NULL)
+        return (0);
+    if (node->right || node->left == NULL)
+        return (1);
+    return (10);
 }
