04391c052ff25f1dd63f62289260bf7cc78be6fb
MarcosPimienta <1676@holbertonschool.com>
2020-11-25 20:08:09 +0000

    delete binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,9 +8,10 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree)
-		return (NULL);
-	free(tree->left);
-	free(tree->right);
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
