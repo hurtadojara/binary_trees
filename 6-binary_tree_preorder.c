3a9d2a1a10a1fa7e6c72d5fd203bdd27889d7b25
MarcosPimienta <1676@holbertonschool.com>
2020-11-25 21:49:46 +0000

    betty fixed

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,19 +2,19 @@
 
 /**
  * binary_tree_preorder -  goes through a binary tree using pre-order traversal
- * @parent: parent to append value
- * @value: value of node
- * Return: New node
+ * @tree: tree
+ * @func: print function
+ * Return: void
  **/
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL)
-                return;
-        if (func == NULL)
-                return;
+	if (tree == NULL)
+		return;
+	if (func == NULL)
+		return;
 
-        func(tree->n);
-        binary_tree_preorder(tree->left, func);
-        binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
