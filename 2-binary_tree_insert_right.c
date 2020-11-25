ed4cecfddc91c312ac3c72f4dcf5fa2e6b65b7c7
MarcosPimienta <1676@holbertonschool.com>
2020-11-25 19:34:31 +0000

    right node add

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - insert in the_right node
+ * binary_tree_insert_right - insert in the right node
  * @parent: parent to append value
  * @value: value of node
  * Return: New node
@@ -9,35 +9,19 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *n_node;
+	binary_tree_t *n_node;
 
-	if (parent)
-        {
-                if (parent->right)
-                {
-                        n_node = malloc(sizeof(binary_tree_t));
-                        if (!n_node)
-                                return (NULL);
+	if (!parent)
+		return (NULL);
 
-                        n_node->parent = parent->right->parent;
-                        n_node->right = parent->right;
-                        n_node->n = value;
-                        parent->right->parent = n_node;
-                        parent->right = n_node;
-                        return(n_node);
-                }
-                else
-                {
-                        n_node = malloc(sizeof(binary_tree_t));
-                        if (!n_node)
-                                return (NULL);
-                        n_node->parent = parent;
-                        n_node->n = value;
-                        n_node->right = NULL;
-                        parent->right = n_node;
-
-                        return (n_node);
-                }
-        }
-        return (NULL);
+		n_node = binary_tree_node(parent, value);
+		if (!n_node)
+			return (NULL);
+		if (parent->right)
+		{
+			parent->right->parent = n_node;
+			n_node->right = parent->right;
+		}
+			parent->right = n_node;
+			return (n_node);
 }
