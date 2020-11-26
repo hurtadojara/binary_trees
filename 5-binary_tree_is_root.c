f4ebbdff7b4e5ef32639107af052c72d678742f3
MarcosPimienta <1676@holbertonschool.com>
2020-11-26 16:18:07 +0000

    fixed betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
- */
+* binary_tree_is_root - binary tree is root
+* @node: - node structure
+* Return: New node
+**/
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node)
-        return (0);
-    if (node->parent == NULL)
-        return (1);
-    return (0);
+	if (!node)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
 }
