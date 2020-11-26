4d4293ca1a20e3eec9e138fa3d29c387a6b9bc70
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-11-26 13:54:06 -0500

    ipdated

--- binary_trees.h
+++ binary_trees.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -39,5 +38,6 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif
